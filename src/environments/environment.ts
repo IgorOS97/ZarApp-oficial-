ba939279f772eac15b08e75a7b58176a0a7c36b3
Damaceno25 <damaceno2590@gmail.com>
2024-11-24 21:16:07 -0300

    authentication
    Autenticação de login e cadastro via Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ firebaseConfig : {
+  apiKey: "AIzaSyBfzBKilZapnJtcgZt8fn4gsd3gyF7AzUU",
+  authDomain: "zarapp-auth.firebaseapp.com",
+  projectId: "zarapp-auth",
+  storageBucket: "zarapp-auth.firebasestorage.app",
+  messagingSenderId: "719382090330",
+  appId: "1:719382090330:web:ddb5c72843a9f5ded47e63",
+  measurementId: "G-SNLGR6SKPF"
+}
+
 };
 
 /*
